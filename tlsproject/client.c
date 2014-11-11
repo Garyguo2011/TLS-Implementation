ae36084c7d96c179fa8435d2aaa76a36c5086d41
garyguo2011 <xguo@berkeley.edu>
2014-11-11 14:19:13 -0800

    Sending Receiving message

--- tlsproject/client.c
+++ tlsproject/client.c
@@ -22,189 +22,189 @@ static int random_int();
 static void cleanup();
 
 int main(int argc, char **argv) {
-  int err, option_index, c, clientlen, counter;
-  unsigned char rcv_plaintext[AES_BLOCK_SIZE];
-  unsigned char rcv_ciphertext[AES_BLOCK_SIZE];
-  unsigned char send_plaintext[AES_BLOCK_SIZE];
-  unsigned char send_ciphertext[AES_BLOCK_SIZE];
-  aes_context enc_ctx, dec_ctx;
-  in_addr_t ip_addr;
-  struct sockaddr_in server_addr;
-  FILE *c_file, *d_file, *m_file;
-  ssize_t read_size, write_size;
-  struct sockaddr_in client_addr;
-  tls_msg err_msg, send_msg, rcv_msg;
-  mpz_t client_exp, client_mod;
-  fd_set readfds;
-  struct timeval tv;
+	int err, option_index, c, clientlen, counter;
+	unsigned char rcv_plaintext[AES_BLOCK_SIZE];
+	unsigned char rcv_ciphertext[AES_BLOCK_SIZE];
+	unsigned char send_plaintext[AES_BLOCK_SIZE];
+	unsigned char send_ciphertext[AES_BLOCK_SIZE];
+	aes_context enc_ctx, dec_ctx;
+	in_addr_t ip_addr;
+	struct sockaddr_in server_addr;
+	FILE *c_file, *d_file, *m_file;
+	ssize_t read_size, write_size;
+	struct sockaddr_in client_addr;
+	tls_msg err_msg, send_msg, rcv_msg;
+	mpz_t client_exp, client_mod;
+	fd_set readfds;
+	struct timeval tv;
 
-  c_file = d_file = m_file = NULL;
+	c_file = d_file = m_file = NULL;
 
-  mpz_init(client_exp);
-  mpz_init(client_mod);
+	mpz_init(client_exp);
+	mpz_init(client_mod);
 
-  /*
-   * This section is networking code that you don't need to worry about.
-   * Look further down in the function for your part.
-   */
+	/*
+	 * This section is networking code that you don't need to worry about.
+	 * Look further down in the function for your part.
+	 */
 
-  memset(&ip_addr, 0, sizeof(in_addr_t));
+	memset(&ip_addr, 0, sizeof(in_addr_t));
 
-  option_index = 0;
-  err = 0;
+	option_index = 0;
+	err = 0;
 
-  static struct option long_options[] = {
-    {"ip", required_argument, 0, 'i'},
-    {"cert", required_argument, 0, 'c'},
-    {"exponent", required_argument, 0, 'd'},
-    {"modulus", required_argument, 0, 'm'},
-    {0, 0, 0, 0},
-  };
+	static struct option long_options[] = {
+		{"ip", required_argument, 0, 'i'},
+		{"cert", required_argument, 0, 'c'},
+		{"exponent", required_argument, 0, 'd'},
+		{"modulus", required_argument, 0, 'm'},
+		{0, 0, 0, 0},
+	};
 
-  while (1) {
-    c = getopt_long(argc, argv, "c:i:d:m:", long_options, &option_index);
-    if (c < 0) {
-      break;
-    }
-    switch(c) {
-    case 0:
-      usage();
-      break;
-    case 'c':
-      c_file = fopen(optarg, "r");
-      if (c_file == NULL) {
-	perror("Certificate file error");
-	exit(1);
-      }
-      break;
-    case 'd':
-      d_file = fopen(optarg, "r");
-      if (d_file == NULL) {
-	perror("Exponent file error");
-	exit(1);
-      }
-      break;
-    case 'i':
-      ip_addr = inet_addr(optarg);
-      break;
-    case 'm':
-      m_file = fopen(optarg, "r");
-      if (m_file == NULL) {
-	perror("Modulus file error");
-	exit(1);
-      }
-      break;
-    case '?':
-      usage();
-      break;
-    default:
-      usage();
-      break;
-    }
-  }
-
-  if (d_file == NULL || c_file == NULL || m_file == NULL) {
-    usage();
-  }
-  if (argc != 9) {
-    usage();
-  }
-
-  mpz_inp_str(client_exp, d_file, 0);
-  mpz_inp_str(client_mod, m_file, 0);
-
-  signal(SIGTERM, kill_handler);
-
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    perror("Could not open socket");
-    exit(1);
-  }
-
-  memset(&server_addr, 0, sizeof(struct sockaddr_in));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = ip_addr;
-  server_addr.sin_port = htons(HANDSHAKE_PORT);
-  err = connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-  if (err < 0) {
-    perror("Could not bind socket");
-    cleanup();
-  }
-
-  // YOUR CODE HERE
-  // IMPLEMENT THE TLS HANDSHAKE
-
-  /*
-   * START ENCRYPTED MESSAGES
-   */
-
-  memset(send_plaintext, 0, AES_BLOCK_SIZE);
-  memset(send_ciphertext, 0, AES_BLOCK_SIZE);
-  memset(rcv_plaintext, 0, AES_BLOCK_SIZE);
-  memset(rcv_ciphertext, 0, AES_BLOCK_SIZE);
-
-  memset(&rcv_msg, 0, TLS_MSG_SIZE);
-
-  aes_init(&enc_ctx);
-  aes_init(&dec_ctx);
-  
-  // YOUR CODE HERE
-  // SET AES KEYS
-
-  fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
-  /* Send and receive data. */
-  while (1) {
-    FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds);
-    FD_SET(sockfd, &readfds);
-    tv.tv_sec = 2;
-    tv.tv_usec = 10;
-
-    select(sockfd+1, &readfds, NULL, NULL, &tv);
-    if (FD_ISSET(STDIN_FILENO, &readfds)) {
-      counter = 0;
-      memset(&send_msg, 0, TLS_MSG_SIZE);
-      send_msg.type = ENCRYPTED_MESSAGE;
-      memset(send_plaintext, 0, AES_BLOCK_SIZE);
-      read_size = read(STDIN_FILENO, send_plaintext, AES_BLOCK_SIZE);
-      while (read_size > 0 && counter + AES_BLOCK_SIZE < TLS_MSG_SIZE - INT_SIZE) {
-	if (read_size > 0) {
-	  err = aes_crypt_ecb(&enc_ctx, AES_ENCRYPT, send_plaintext, send_ciphertext);
-	  memcpy(send_msg.msg + counter, send_ciphertext, AES_BLOCK_SIZE);
-	  counter += AES_BLOCK_SIZE;
+	while (1) {
+		c = getopt_long(argc, argv, "c:i:d:m:", long_options, &option_index);
+		if (c < 0) {
+			break;
+		}
+		switch(c) {
+		case 0:
+			usage();
+			break;
+		case 'c':
+			c_file = fopen(optarg, "r");
+			if (c_file == NULL) {
+				perror("Certificate file error");
+				exit(1);
+			}
+			break;
+		case 'd':
+			d_file = fopen(optarg, "r");
+			if (d_file == NULL) {
+				perror("Exponent file error");
+				exit(1);
+			}
+			break;
+		case 'i':
+			ip_addr = inet_addr(optarg);
+			break;
+		case 'm':
+			m_file = fopen(optarg, "r");
+			if (m_file == NULL) {
+				perror("Modulus file error");
+				exit(1);
+			}
+			break;
+		case '?':
+			usage();
+			break;
+		default:
+			usage();
+			break;
+		}
 	}
+
+	if (d_file == NULL || c_file == NULL || m_file == NULL) {
+		usage();
+	}
+	if (argc != 9) {
+		usage();
+	}
+
+	mpz_inp_str(client_exp, d_file, 0);
+	mpz_inp_str(client_mod, m_file, 0);
+
+	signal(SIGTERM, kill_handler);
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		perror("Could not open socket");
+		exit(1);
+	}
+
+	memset(&server_addr, 0, sizeof(struct sockaddr_in));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = ip_addr;
+	server_addr.sin_port = htons(HANDSHAKE_PORT);
+	err = connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+	if (err < 0) {
+		perror("Could not bind socket");
+		cleanup();
+	}
+
+	// YOUR CODE HERE
+	// IMPLEMENT THE TLS HANDSHAKE
+
+	/*
+	 * START ENCRYPTED MESSAGES
+	 */
+
 	memset(send_plaintext, 0, AES_BLOCK_SIZE);
-	read_size = read(STDIN_FILENO, send_plaintext, AES_BLOCK_SIZE);
-      }
-      write_size = write(sockfd, &send_msg, INT_SIZE+counter+AES_BLOCK_SIZE);
-      if (write_size < 0) {
-	perror("Could not write to socket");
-	cleanup();
-      }
-    } else if (FD_ISSET(sockfd, &readfds)) {
-      memset(&rcv_msg, 0, TLS_MSG_SIZE);
-      memset(rcv_ciphertext, 0, AES_BLOCK_SIZE);
-      read_size = read(sockfd, &rcv_msg, TLS_MSG_SIZE);
-      if (read_size > 0) {
-	if (rcv_msg.type != ENCRYPTED_MESSAGE) {
-	  goto out;
-	}
-	memcpy(rcv_ciphertext, rcv_msg.msg, AES_BLOCK_SIZE);
-	counter = 0;
-	while (counter < read_size - INT_SIZE - AES_BLOCK_SIZE) {
-	  aes_crypt_ecb(&dec_ctx, AES_DECRYPT, rcv_ciphertext, rcv_plaintext);
-	  printf("%s", rcv_plaintext);
-	  counter += AES_BLOCK_SIZE;
-	  memcpy(rcv_ciphertext, rcv_msg.msg+counter, AES_BLOCK_SIZE);
-	}
-      }
-    }
+	memset(send_ciphertext, 0, AES_BLOCK_SIZE);
+	memset(rcv_plaintext, 0, AES_BLOCK_SIZE);
+	memset(rcv_ciphertext, 0, AES_BLOCK_SIZE);
 
-  }
+	memset(&rcv_msg, 0, TLS_MSG_SIZE);
+
+	aes_init(&enc_ctx);
+	aes_init(&dec_ctx);
+	
+	// YOUR CODE HERE
+	// SET AES KEYS
+
+	fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+	/* Send and receive data. */
+	while (1) {
+		FD_ZERO(&readfds);
+		FD_SET(STDIN_FILENO, &readfds);
+		FD_SET(sockfd, &readfds);
+		tv.tv_sec = 2;
+		tv.tv_usec = 10;
+
+		select(sockfd+1, &readfds, NULL, NULL, &tv);
+		if (FD_ISSET(STDIN_FILENO, &readfds)) {
+			counter = 0;
+			memset(&send_msg, 0, TLS_MSG_SIZE);
+			send_msg.type = ENCRYPTED_MESSAGE;
+			memset(send_plaintext, 0, AES_BLOCK_SIZE);
+			read_size = read(STDIN_FILENO, send_plaintext, AES_BLOCK_SIZE);
+			while (read_size > 0 && counter + AES_BLOCK_SIZE < TLS_MSG_SIZE - INT_SIZE) {
+				if (read_size > 0) {
+					err = aes_crypt_ecb(&enc_ctx, AES_ENCRYPT, send_plaintext, send_ciphertext);
+					memcpy(send_msg.msg + counter, send_ciphertext, AES_BLOCK_SIZE);
+					counter += AES_BLOCK_SIZE;
+				}
+				memset(send_plaintext, 0, AES_BLOCK_SIZE);
+				read_size = read(STDIN_FILENO, send_plaintext, AES_BLOCK_SIZE);
+			}
+			write_size = write(sockfd, &send_msg, INT_SIZE+counter+AES_BLOCK_SIZE);
+			if (write_size < 0) {
+				perror("Could not write to socket");
+				cleanup();
+			}
+		} else if (FD_ISSET(sockfd, &readfds)) {
+			memset(&rcv_msg, 0, TLS_MSG_SIZE);
+			memset(rcv_ciphertext, 0, AES_BLOCK_SIZE);
+			read_size = read(sockfd, &rcv_msg, TLS_MSG_SIZE);
+			if (read_size > 0) {
+				if (rcv_msg.type != ENCRYPTED_MESSAGE) {
+					goto out;
+				}
+				memcpy(rcv_ciphertext, rcv_msg.msg, AES_BLOCK_SIZE);
+				counter = 0;
+				while (counter < read_size - INT_SIZE - AES_BLOCK_SIZE) {
+					aes_crypt_ecb(&dec_ctx, AES_DECRYPT, rcv_ciphertext, rcv_plaintext);
+					printf("%s", rcv_plaintext);
+					counter += AES_BLOCK_SIZE;
+					memcpy(rcv_ciphertext, rcv_msg.msg+counter, AES_BLOCK_SIZE);
+				}
+			}
+		}
+
+	}
 
  out:
-  close(sockfd);
-  return 0;
+	close(sockfd);
+	return 0;
 }
 
 /*
@@ -221,8 +221,8 @@ int main(int argc, char **argv) {
  */
 void
 decrypt_cert(mpz_t decrypted_cert, cert_message *cert, mpz_t key_exp, mpz_t key_mod)
-{
-  // YOUR CODE HERE
+{0
+	// YOUR CODE HERE
 }
 
 /*
@@ -240,7 +240,7 @@ decrypt_cert(mpz_t decrypted_cert, cert_message *cert, mpz_t key_exp, mpz_t key_
 void
 decrypt_verify_master_secret(mpz_t decrypted_ms, ps_msg *ms_ver, mpz_t key_exp, mpz_t key_mod)
 {
-  // YOUR CODE HERE
+	// YOUR CODE HERE
 }
 
 /*
@@ -255,7 +255,7 @@ decrypt_verify_master_secret(mpz_t decrypted_ms, ps_msg *ms_ver, mpz_t key_exp, 
 void
 compute_master_secret(int ps, int client_random, int server_random, char *master_secret)
 {
-  // YOUR CODE HERE
+	// YOUR CODE HERE
 }
 
 /*
@@ -270,7 +270,13 @@ compute_master_secret(int ps, int client_random, int server_random, char *master
 int
 send_tls_message(int socketno, void *msg, int msg_len)
 {
-  // YOUR CODE HERE
+	// YOUR CODE HERE
+	// n = write(socketno, msg, msg_len);
+	// if (n < 0){
+	// 	return ERR_FAILURE;
+	// }else{
+	// 	return ERR_OK;
+	// }
 }
 
 /*
@@ -286,7 +292,28 @@ send_tls_message(int socketno, void *msg, int msg_len)
 int
 receive_tls_message(int socketno, void *msg, int msg_len, int msg_type)
 {
-  // YOUR CODE HERE
+	// YOUR CODE HERE
+	// Need to deal with the case that receive message is ERROR message
+	// if (msg_type == ERROR_MESSAGE){
+
+	// }
+	// Mesage length probabaly large than MAX_REc
+	// Need while loop here
+	// void *msg_ptr = msg;
+	// int remain_bytes = msg_len;
+	// while (remain_bytes > MAX_RECEIVE_BYTES){
+	// 	n = read(socketno, msg_ptr, MAX_RECEIVE_BYTES)
+	// 	if (n < 0){
+	// 		return ERR_FAILURE;
+	// 	}
+	// 	remain_bytes -= MAX_RECEIVE_BYTES;
+	// 	msg_ptr += MAX_RECEIVE_BYTES;
+	// }
+	// n = read(socketno, msg_ptr, remain_bytes)
+	// if (n < 0){
+	// 	return ERR_FAILURE;
+	// }
+	// return ERR_OK
 }
 
 
@@ -305,6 +332,44 @@ receive_tls_message(int socketno, void *msg, int msg_len, int msg_type)
 static void
 perform_rsa(mpz_t result, mpz_t message, mpz_t e, mpz_t n)
 {
+	int hex = 16;
+	mpz_t zero, one, two, tmp;
+	char zero_str[] = "0";
+	char one_str[] = "1";
+	char two_str[] = "2";
+
+	mpz_init(zero);
+	mpz_init(one);
+	mpz_init(two);
+	mpz_init(tmp);
+
+	mpz_set_str(zero, zero_str, hex);
+	mpz_set_str(one, one_str, hex);
+	mpz_set_str(two, two_str, hex);
+	mpz_set_str(tmp, zero_str, hex);
+	
+	// initilize result = 1;
+	mpz_add(result, zero, one);
+
+	while (mpz_cmp(d, zero) > 0){
+		mpz_mod(tmp, d, two);
+		if (mpz_cmp(tmp, one) == 0) {
+			// result = (result * message) % n;
+			mpz_mul(tmp, result, message);
+			mpz_mod(result, tmp, n);
+			// d--;
+			mpz_sub(d, d, one);
+		}
+		// d = d/2;
+		mpz_div(d, d, two);
+		// message = (message * message) % n;
+		mpz_mul(tmp, message, message);
+		mpz_mod(message, tmp, n);
+	}
+	mpz_clear(zero);
+	mpz_clear(one);
+	mpz_clear(two);
+	mpz_clear(tmp);
 }
 
 
@@ -312,8 +377,8 @@ perform_rsa(mpz_t result, mpz_t message, mpz_t e, mpz_t n)
 static int
 random_int()
 {
-  srand(time(NULL));
-  return rand();
+	srand(time(NULL));
+	return rand();
 }
 
 /*
@@ -325,16 +390,16 @@ random_int()
 void
 mpz_get_ascii(char *output_str, mpz_t input)
 {
-  int i,j;
-  char *result_str;
-  result_str = mpz_get_str(NULL, HEX_BASE, input);
-  i = 0;
-  j = 0;
-  while (result_str[i] != '\0') {
-    output_str[j] = hex_to_ascii(result_str[i], result_str[i+1]);
-    j += 1;
-    i += 2;
-  }
+	int i,j;
+	char *result_str;
+	result_str = mpz_get_str(NULL, HEX_BASE, input);
+	i = 0;
+	j = 0;
+	while (result_str[i] != '\0') {
+		output_str[j] = hex_to_ascii(result_str[i], result_str[i+1]);
+		j += 1;
+		i += 2;
+	}
 }
 
 /*
@@ -347,98 +412,98 @@ mpz_get_ascii(char *output_str, mpz_t input)
 char
 *hex_to_str(char *data, int data_len)
 {
-  int i;
-  char *output_str = calloc(1+2*data_len, sizeof(char));
-  for (i = 0; i < data_len; i += 1) {
-    snprintf(output_str+2*i, 3, "%02X", (unsigned int) (data[i] & 0xFF));
-  }
-  return output_str;
+	int i;
+	char *output_str = calloc(1+2*data_len, sizeof(char));
+	for (i = 0; i < data_len; i += 1) {
+		snprintf(output_str+2*i, 3, "%02X", (unsigned int) (data[i] & 0xFF));
+	}
+	return output_str;
 }
 
 /* Return the public key exponent given the decrypted certificate as string. */
 void
 get_cert_exponent(mpz_t result, char *cert)
 {
-  char *srch, *srch2;
-  char exponent[RSA_MAX_LEN/2];
-  memset(exponent, 0, RSA_MAX_LEN/2);
-  srch = strchr(cert, '\n');
-  srch += 1;
-  srch = strchr(srch, '\n');
-  srch += 1;
-  srch = strchr(srch, '\n');
-  srch += 1;
-  srch = strchr(srch, ':');
-  srch += 2;
-  srch2 = strchr(srch, '\n');
-  strncpy(exponent, srch, srch2-srch);
-  mpz_set_str(result, exponent, 0);
+	char *srch, *srch2;
+	char exponent[RSA_MAX_LEN/2];
+	memset(exponent, 0, RSA_MAX_LEN/2);
+	srch = strchr(cert, '\n');
+	srch += 1;
+	srch = strchr(srch, '\n');
+	srch += 1;
+	srch = strchr(srch, '\n');
+	srch += 1;
+	srch = strchr(srch, ':');
+	srch += 2;
+	srch2 = strchr(srch, '\n');
+	strncpy(exponent, srch, srch2-srch);
+	mpz_set_str(result, exponent, 0);
 }
 
 /* Return the public key modulus given the decrypted certificate as string. */
 void
 get_cert_modulus(mpz_t result, char *cert)
 {
-  char *srch, *srch2;
-  char modulus[RSA_MAX_LEN/2];
-  memset(modulus, 0, RSA_MAX_LEN/2);
-  srch = strchr(cert, '\n');
-  srch += 1;
-  srch = strchr(srch, '\n');
-  srch += 1;
-  srch = strchr(srch, ':');
-  srch += 2;
-  srch2 = strchr(srch, '\n');
-  strncpy(modulus, srch, srch2-srch);
-  mpz_set_str(result, modulus, 0);
+	char *srch, *srch2;
+	char modulus[RSA_MAX_LEN/2];
+	memset(modulus, 0, RSA_MAX_LEN/2);
+	srch = strchr(cert, '\n');
+	srch += 1;
+	srch = strchr(srch, '\n');
+	srch += 1;
+	srch = strchr(srch, ':');
+	srch += 2;
+	srch2 = strchr(srch, '\n');
+	strncpy(modulus, srch, srch2-srch);
+	mpz_set_str(result, modulus, 0);
 }
 
 /* Prints the usage string for this program and exits. */
 static void
 usage()
 {
-    printf("./client -i <server_ip_address> -c <certificate_file> -m <modulus_file> -d <exponent_file>\n");
-    exit(1);
+	printf("./client -i <server_ip_address> -c <certificate_file> -m <modulus_file> -d <exponent_file>\n");
+	exit(1);
 }
 
 /* Catches the signal from C-c and closes connection with server. */
 static void
 kill_handler(int signum)
 {
-  if (signum == SIGTERM) {
-    cleanup();
-  }
+	if (signum == SIGTERM) {
+		cleanup();
+	}
 }
 
 /* Converts the two input hex characters into an ascii char. */
 static int
 hex_to_ascii(char a, char b)
 {
-    int high = hex_to_int(a) * 16;
-    int low = hex_to_int(b);
-    return high + low;
+	int high = hex_to_int(a) * 16;
+	int low = hex_to_int(b);
+	return high + low;
 }
 
 /* Converts a hex value into an int. */
 static int
 hex_to_int(char a)
 {
-    if (a >= 97) {
-	a -= 32;
-    }
-    int first = a / 16 - 3;
-    int second = a % 16;
-    int result = first*10 + second;
-    if (result > 9) {
-	result -= 1;
-    }
-    return result;
+	if (a >= 97) {
+		a -= 32;
+	}
+	int first = a / 16 - 3;
+	int second = a % 16;
+	int result = first*10 + second;
+	if (result > 9) {
+		result -= 1;
+	}
+	return result;
 }
 
 /* Closes files and exits the program. */
 static void
 cleanup()
 {
-  close(sockfd);
-  exit(1);
+	close(sockfd);
+	exit(1);
 }
